531b03f14a93af188e49849896105c86564c518a
Dario Ajlin <DGAjlin@teco.com.ar>
2022-02-11 17:23:29 -0300

    commiteo Clase 11

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,21 +1,35 @@
 import { useContext } from 'react'
 import { CartContext } from "../components/CartContext"
+import { Link } from 'react-router-dom';
 
 // Carrito de Compras
 const Cart = () => {
     const elementos = useContext(CartContext);
+ 
+    // Obtengo los Subtotales
+    let subtotal = 0;
+    elementos.cartList.map(item => (
+        subtotal = subtotal + (item.costo * item.cantidad)
+    ))
+
     return (
         <>
-            <div className="contVaciarCarrito">                
+            <div className="contVaciarCarrito">               
                 {
                     elementos.cartList.length !== 0
-                    ? <button type="button" className="btnCheckout" onClick={elementos.clear}>Vaciar Carrito</button>
-                    : <div></div>
+                    ? <div></div>
+                    : <div>                        
+                        <div className="carritoVacio">
+                            <label className="lblCarritoVacio">El carrito de compras está vacio</label>
+                        </div>
+                        <div className="contContinuar">
+                            <Link to='/'><button type="button" className="btnContinuar">Volver al Inicio</button></Link>
+                        </div>                    
+                      </div>
                 }
             </div>
-
             {
-                elementos.cartList.map(item => (
+                elementos.cartList.map(item => (                    
                     <div className="contenedor1">
                         <div className="columna1">
                             <h3 className="nombreDestinoCart">{item.title}</h3>
@@ -27,11 +41,33 @@ const Cart = () => {
                         <div className="columna2">
                             <div className="costoCart">
                                 Costo: ${item.costo}/pasajero<br/>
-                                Pasajeros: {item.cantidad}
+                                Pasajeros: {item.cantidad}   
                             </div>                        
-                        </div>                                                    
+                        </div>          
                     </div>                    
                 ))
+            }
+            {
+                subtotal !== 0
+                ? <div className="subtotal">
+                    <div class="contResumen">
+                        <label className="txtResumen">Resumen</label>
+                    </div>
+                    <label className="txtSubtotal">Subtotal: ${subtotal}</label><br/>
+                    <label className="txtImpuestos">Impuestos: ${subtotal * 0.105}</label><br/>
+                    <label className="txtImpuestos">Descuentos: -${subtotal * 0.105}</label><br/><br/>
+                    <label className="txtTotal">Total: ${subtotal + (subtotal * 0.105) - (subtotal * 0.105)} </label>
+                    <div className="contContinuar">
+                        <Link to='/'><button type="button" className="btnContinuar">Continuar Comprando</button></Link>
+                    </div>
+                    <div className="contVaciar">
+                        <button type="button" className="btnVaciar" onClick={elementos.clear}>Vaciar Carrito</button>
+                    </div>
+                    <div className="contTerminar">
+                        <button type="button" className="btnComprar">Terminar Compra</button>
+                    </div>
+                 </div>
+                : <div></div>
             }
         </>
     );
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,20 +1,34 @@
 // Contenedor del Carrito de Compras
 import Carrito from "./images/carrito.jpg";
-
-let cantitems = 4;
+import { useContext } from 'react'
+import { CartContext } from "../components/CartContext"
+import { Link } from "react-router-dom";
 
 const CartWidget = () => {
+    const elementos = useContext(CartContext);
+
+    // Obtengo la cantidad de Items del carrito
+    let cantitems = 0;
+    elementos.cartList.map(items => (
+        cantitems += items.cantidad
+    ))
+
     return (
         <>
-            <div className="contcarrito">
-                <div className="imagen">
-                    <img alt="Carrito" src={Carrito} className="imgCarrito" id="btnCarrito" />
+            {
+                cantitems > 0
+                ?
+                <div className="contcarrito">
+                    <div className="imagen">
+                        <Link to='/cart' style={{background: 'transparent'}}><img alt="Carrito" src={Carrito} className="imgCarrito" /></Link>
+                    </div>
+                    <div className="items">
+                        <label className="cantdestinos" id="cantDestinos">{cantitems}</label>
+                        <label className="lbldestinos">&nbsp;items</label>
+                    </div>
                 </div>
-                <div className="items">
-                    <label className="cantdestinos" id="cantDestinos">{cantitems}</label>
-                    <label className="lbldestinos">&nbsp;items</label>
-                </div>
-            </div>
+                :<div></div>
+            }
         </>
     );
 }
